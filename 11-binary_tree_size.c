df312b39bff27922e0f449d5f1aa16bcf5d5f38c
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-01-15 14:48:43 +0100

    measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,11 +7,7 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size=0;
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
+	if (tree)
 	{
 		size += 1;
 		size += binary_tree_size(tree->left);
